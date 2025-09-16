2d14c2b0480b304247ce175e131fc1fb03c2c589
VazMF <vazfernandam@gmail.com>
2025-09-15 23:13:24 -0300

    style: using fragment and removing unnecessary div

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -60,7 +60,7 @@ function App() {
           )}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
